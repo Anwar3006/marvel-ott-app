703f348e934ca9587e6578aa83d0cf718eb98849
Anwar3006 <anwarsadat.d2@gmail.com>
2024-01-25 12:21:40 +0000

    modified middleware to get cookies "_Secure...."

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const cookie = request.cookies.get("next-auth.session-token");
+  const cookie = request.cookies.get("__Secure-next-auth.session-token");
+
   if (cookie) {
     return NextResponse.next();
   }
